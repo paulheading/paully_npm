fdd65ae4143fe16003c97f4c3a32f5c9e1b5e6b5
Paul Heading <paulheading@me.com>
2024-11-22 17:33:29 +0000

    more svgs

--- svgs/index.js
+++ svgs/index.js
@@ -1,34 +1,49 @@
+import About from "#svgs/about";
 import Back from "#svgs/back";
 import Close from "#svgs/close";
+import Confetti from "#svgs/confetti";
 import Dots from "#svgs/dots";
 import Down from "#svgs/down";
+import Graph from "#svgs/graph";
 import Info from "#svgs/info";
 import Mute from "#svgs/mute";
 import Next from "#svgs/next";
 import Pause from "#svgs/pause";
 import Play from "#svgs/play";
 import Previous from "#svgs/previous";
+import Resume from "#svgs/resume";
 import Spotify from "#svgs/spotify";
 import Unmute from "#svgs/unmute";
 import Up from "#svgs/up";
+import Wifi from "#svgs/wifi";
 
 import LargePaully from "#svgs/paully/large";
 import SmallPaully from "#svgs/paully/small";
 
+import OpenFolder from "#svgs/folder/open";
+import ClosedFolder from "#svgs/folder/closed";
+
 export {
+  About,
   Back,
   Close,
+  Confetti,
   Dots,
   Down,
+  Graph,
   Info,
   Mute,
   Next,
   Pause,
   Play,
   Previous,
+  Resume,
   Spotify,
   Unmute,
   Up,
+  Wifi,
   LargePaully,
   SmallPaully,
+  OpenFolder,
+  ClosedFolder,
 };
