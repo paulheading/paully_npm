57d7487f344bcd290d60fe3cc831a3ab087ffc26
Paul Heading <paulheading@me.com>
2024-11-27 17:35:09 +0000

    loader improvements

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import BackLink from "#components/links/back";
 import Banner from "#components/banner";
+import Container from "#components/container";
 import InvisibleButton from "#components/buttons/invisible";
 import Label from "#components/label";
 import Labels from "#components/labels";
@@ -14,6 +15,7 @@ import Wrap from "#components/wrap";
 export {
   BackLink,
   Banner,
+  Container,
   InvisibleButton,
   Label,
   Labels,
